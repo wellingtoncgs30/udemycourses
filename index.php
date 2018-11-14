e042e401b9fc1932b2b4dcd5ea7ba6ac96dca1f4
wellingtoncgs30 <43141242+wellingtoncgs30@users.noreply.github.com>
2018-11-14 20:30:05 -0200

    Add files via upload

--- index.php
+++ index.php
@@ -44,7 +44,37 @@ $app->get(
 
 	$data = $sql->select("SELECT * FROM tb_produtos where preco_promorcional > 0 order by preco_promorcional desc limit 3;");
 
+	foreach($data as &$produto){
+		$preco = $produto['preco'];
+		$centavos = explode(".", $preco);
+		$produto['preco'] = number_format($preco, 0, ",", ".");
+		$produto['centavos'] = end($centavos);
+		$produto['parcelas'] = 10;
+		$produto['parcela'] = number_format($preco/$produto['parcelas'], 2, ",",".");
+		$produto['total'] = number_format($preco, 2, ",", ".");
+	}
+
 	echo json_encode($data);
+
+});
+
+$app->get('/produtos-mais-buscados', function(){
+
+	$sql = new Sql();
+
+	$data = $sql->select("SELECT * FROM tb_produtos LIMIT 4;");
+
+	foreach($data as &$produto){
+		$preco = $produto['preco'];
+		$centavos = explode(".", $preco);
+		$produto['preco'] = number_format($preco, 0, ",", ".");
+		$produto['centavos'] = end($centavos);
+		$produto['parcelas'] = 10;
+		$produto['parcela'] = number_format($preco/$produto['parcelas'], 2, ",",".");
+		$produto['total'] = number_format($preco, 2, ",", ".");
+	}
+
+	echo json_encode($data);	
 
 });
 
