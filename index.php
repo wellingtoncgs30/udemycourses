3663d5e9fdc8a0ae18ff7f0d46d6a5440ec0e403
wellingtoncgs30 <43141242+wellingtoncgs30@users.noreply.github.com>
2018-11-29 20:21:51 -0200

    Add files via upload

--- index.php
+++ index.php
@@ -1,7 +1,6 @@
 <?php
 
 require 'inc/configuration.php';
-
 require 'inc/Slim-2.x/Slim/Slim.php';
 
 \Slim\Slim::registerAutoloader();
@@ -36,9 +35,7 @@ $app->get(
 	}
 );
 
-$app->get(
-	'/produtos',
-	function(){
+$app->get('/produtos', function(){
 
 	$sql = new Sql();
 
@@ -62,7 +59,41 @@ $app->get('/produtos-mais-buscados', function(){
 
 	$sql = new Sql();
 
-	$data = $sql->select("SELECT * FROM tb_produtos LIMIT 4;");
+	$data = $sql->select("
+        SELECT 
+        tb_produtos.id_prod,
+        tb_produtos.nome_prod_curto,
+        tb_produtos.nome_prod_longo,
+        tb_produtos.codigo_interno,
+        tb_produtos.id_cat,
+        tb_produtos.preco,
+        tb_produtos.peso,
+        tb_produtos.largura_centimetro,
+        tb_produtos.altura_centimetro,
+        tb_produtos.quantidade_estoque,
+        tb_produtos.preco_promorcional,
+        tb_produtos.foto_principal,
+        tb_produtos.visivel,
+        cast(avg(review) as dec(10,2)) as media, 
+        count(id_prod) as total_reviews
+        FROM tb_produtos 
+        INNER JOIN tb_reviews USING(id_prod) 
+        GROUP BY 
+        tb_produtos.id_prod,
+        tb_produtos.nome_prod_curto,
+        tb_produtos.nome_prod_longo,
+        tb_produtos.codigo_interno,
+        tb_produtos.id_cat,
+        tb_produtos.preco,
+        tb_produtos.peso,
+        tb_produtos.largura_centimetro,
+        tb_produtos.altura_centimetro,
+        tb_produtos.quantidade_estoque,
+        tb_produtos.preco_promorcional,
+        tb_produtos.foto_principal,
+        tb_produtos.visivel
+        LIMIT 4;
+    ");
 
 	foreach($data as &$produto){
 		$preco = $produto['preco'];
@@ -75,6 +106,18 @@ $app->get('/produtos-mais-buscados', function(){
 	}
 
 	echo json_encode($data);	
+
+});
+
+$app->get("/produto-:id_prod", function($id_prod){
+
+	$sql = new Sql();
+
+	$produtos = $sql->select("SELECT * FROM tb_produtos WHERE id_prod = $id_prod");
+
+	$produto = $produtos[0];
+
+	require_once("view/shop-produto.php");
 
 });
 
